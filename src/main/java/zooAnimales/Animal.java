1eeb8c512fec83a320585fcdadccb7ad7f3e93f4
Santiago S <sanchezsantiago1001@gmail.com>
2024-12-09 22:44:12 -0500

    commit 2

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -28,7 +28,7 @@ public class Animal{
        return "desplazarse";
     }
     public static String totalPorTipo(){
-        return "Mamiferos: " + (Mamifero.cantidadMamiferos()+1) + "\n"+"Aves: " + (Ave.cantidadAves()+2) + "\n"+"Reptiles: " + (Reptil.cantidadReptiles()+1) + "\n" + "Peces: " + (Pez.cantidadPeces()+1) + "\n"+"Anfibios: " + (Anfibio.cantidadAnfibios()+1);
+        return "Mamiferos: " + (Mamifero.cantidadMamiferos()) + "\n"+"Aves: " + (Ave.cantidadAves()) + "\n"+"Reptiles: " + (Reptil.cantidadReptiles()) + "\n" + "Peces: " + (Pez.cantidadPeces()) + "\n"+"Anfibios: " + (Anfibio.cantidadAnfibios());
     }
     public String getNombre(){return nombre;}
     public String getHabitat(){return habitat;}
